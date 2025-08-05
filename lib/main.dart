a8e789cc8ee06c87034d1f444c43dac030c9f868
mhsuhail00 <mhsuhail00@gmail.com>
2025-08-05 18:41:39 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
